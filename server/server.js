5f79e2fc9836d67cee2f257e72ff2f3ffeafa246
Sean <itsmesean@pm.me>
2020-10-30 20:42:49 -0700

    Added express server and basic webpack

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+const express = require("express");
+const path = require("path");
+
+const {PORT} = process.env
+const app = express();
+
+app.use('/dist',express.static(path.resolve(__dirname,"../dist")))
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname,"../client/index.html"))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port:${PORT}`)
+})
