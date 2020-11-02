c7e75e9e807be44b746c9689496f3a93827c26b0
Anthony Magallanes <anthonymagallanes24@gmail.com>
2020-11-01 16:55:34 -0800

    Styled the signup page. Still stuck on post request with name and password using axios.

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,14 +1,49 @@
 import React, { useState } from "react";
+import axios from "axios";
 
-const Login = () => {
+function Login() {
+  const [login, setLogin] = useState({ username: "", password: "" });
+
+  const { username, password } = login;
+
+  function loginDetails(e) {
+    e.preventDefault();
+    console.log("Im inside loginDetails");
+    axios
+      .post("/api/user", login)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   return (
-    <div>
-      <input type="text" placeholder="name" />
-      <input type="password" placeholder="enter password" />
-      <button type="button">Signup</button>
-      <button type="button">Login in</button>
+    <div className="login-form">
+      <div className="sign-up-title">
+        <p>Sign up for a free account</p>
+      </div>
+      <div className="form-details">
+        <form onSubmit={loginDetails}>
+          <input
+            type="text"
+            className="login-field"
+            placeholder="Username"
+            value={username || ""}
+            onChange={(e) => setLogin({ ...login, username: e.target.value })}
+          />
+          <input
+            type="password"
+            className="login-field"
+            id="password-field"
+            placeholder="Password"
+            value={password || ""}
+            onChange={(e) => setLogin({ ...login, password: e.target.value })}
+          />
+        </form>
+      </div>
     </div>
   );
-};
+}
 
 export default Login;
